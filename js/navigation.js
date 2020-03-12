5e7ccf5bbfd96236885087f9fd73080ff0ee25de
showtag <code@showtag.tv>
2020-03-12 12:26:49 +0100

    top navigation added

--- js/navigation.js
+++ js/navigation.js
@@ -1,22 +1,36 @@
-/*---------------------------------------*/
-/*	NAVIGATION AND NAVIGATION VISIBLE ON SCROLL
-/*---------------------------------------*/
-$(window).on("load", function() {
-    mainNav();
-    $(window).scroll(function() {
-        mainNav();
-    });
+//Header
+$(window).scroll(function() {
 
-    function mainNav() {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.main-menu').stop().animate({
-            "opacity": '1',
-            "top": '0'
-        });
-        else $('.main-menu').stop().animate({
-            "top": '-70',
-            "opacity": '0'
-        });
-
+    if ($(window).scrollTop() > 100) {
+        $('.main-header').addClass('sticky');
+    } else {
+        $('.main-header').removeClass('sticky');
     }
+});
+
+// Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main-header').hasClass('open-nav')) {
+        $('.main-header').removeClass('open-nav');
+    } else {
+        $('.main-header').addClass('open-nav');
+    }
+});
+
+$('.main-header li a').click(function() {
+    if ($('.main-header').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main-header').removeClass('open-nav');
+    }
+});
+
+
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
 });

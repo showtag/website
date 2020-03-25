bddb97bfc7c89db61bd39abd0afad0bbafcb44da
showtag <61790349+showtag@users.noreply.github.com>
2020-03-25 15:16:36 +0100

    added sliding banner on landing page

--- js/roles.js
+++ js/roles.js
@@ -1,10 +1,25 @@
 $(function() {
     count = 0;
-    wordsArray = ["Enterprises", "Brands", "Content creators", "Filmmakers", "Brand advocates", "Animators", "Communicators", "Influencers", "Experts", "Hobbyists", "Storytellers"];
+    c = 0;
+    wordsArray = ["Brands", "Content creators", "Filmmakers", "Brand advocates", "Animators", "Communicators", "Influencers", "Experts", "Hobbyists", "Storytellers", "Enterprises"];
+    imagesArray = ["./img/slides/brands.png", "./img/slides/contentc.png", "./img/slides/filmmakers.png",
+        "./img/slides/brandadvocates.png", "./img/slides/animators.png", "./img/slides/communicators.png", "./img/slides/influencers.png",
+        "./img/slides/experts.png", "./img/slides/hobbyists.png", "./img/slides/storytellers.png", "./img/slides/enterprise.png"
+    ];
+
     setInterval(function() {
         count++;
+        var img;
+        console.log("count", count);
+        console.log(wordsArray.length);
+        c = count % wordsArray.length;
         $("#roles").fadeOut(2000, function() {
-            $(this).text(wordsArray[count % wordsArray.length]).fadeIn(2000);
+            console.log(c);
+            $(this).text(wordsArray[c]).fadeIn(2000);
+            img = "'" + imagesArray[c] + "'";
+            console.log(img);
+            $('.splash-container').css({ backgroundImage: 'url(' + img + ')' }).fadeIn(2000);
+            //$('.splash-container').css({ backgroundImage: 'url(' + img + ')' }).fadeIn(2000);
         });
-    }, 2000);
+    }, 4000);
 });
